48d6bf7524505a633253d131eedfe9af4aa2537c
Kris Brandow <kris.brandow@gmail.com>
2026-02-07 17:13:27 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/skriptble/interdep2
+
+go 1.25.4
+
+require github.com/skriptble/dependy/v2 v2.0.2 // indirect
---
+++ interdep2.go
@@ -0,0 +1,7 @@
+package interdep2
+
+import "github.com/skriptble/dependy/v2"
+
+func InterDependency() {
+	dependy.Dependent()
+}
